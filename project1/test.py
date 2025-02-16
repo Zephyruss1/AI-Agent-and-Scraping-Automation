551ffcd26e095b06405159094ce545074068056d
zephyrus <zephyrus1175@yahoo.com>
2025-02-16 15:28:41 +0000

    chatgpt interface

---
+++ project1/test.py
@@ -0,0 +1,133 @@
+from openpyxl import load_workbook
+import os
+import urllib.request
+import concurrent.futures
+from playwright.async_api import async_playwright
+import os
+import time
+from typing import Optional
+import asyncio
+
+class DownloadPDF:
+    def __init__(self, excel_path):
+        self.excel_path = excel_path
+
+    def _load_excel(self):
+        wb = load_workbook(self.excel_path)
+        ws = wb.active
+        if ws:
+            return ws
+
+
+    def download_arxiv_pdf(self, pdf_url: str, save_dir: str ="pdfs"):
+        if not pdf_url:
+            print(f"⚠️ [WARNING] Skipping invalid paper url: {pdf_url}")
+            return False
+
+        if "arxiv.org/abs" in pdf_url:
+            pdf_url = pdf_url.replace("arxiv.org/abs", "arxiv.org/pdf") + ".pdf"
+
+        save_path = os.path.join(save_dir, f"{pdf_url.split('/')[-1]}")
+
+        os.makedirs(save_dir, exist_ok=True)
+
+        try:
+            req = urllib.request.Request(pdf_url, headers={"User-Agent": "Mozilla/5.0"})
+            with urllib.request.urlopen(req) as response:
+                content_type = response.headers.get("Content-Type")
+
+                if content_type != "application/pdf":
+                    print(f"❌ [ERROR] {pdf_url}: PDF can't be downloaded. Content-Type: {content_type}")
+                    return False
+
+                if pdf_url.split("/")[-1] in os.listdir(save_dir):
+                    print(f"⚠️ [WARNING] {pdf_url}: PDF already exists in the directory.")
+                    return False
+
+                with open(save_path, "wb") as f:
+                    f.write(response.read())
+
+            print(f"    ✅ [INFO] {pdf_url}: PDF successfully downloaded -> {save_path}")
+            return True
+
+        except Exception as e:
+            print(f"❌ [ERROR] {pdf_url}: {e}")
+            return False
+
+    def start_download(self, max_workers=3):
+        print("\n📍 Step 6: Downloading PDFs!")
+        ws = self._load_excel()
+        if not ws:
+            print("❌ [ERROR] Excel file not found!")
+            return
+        pdf_urls = [row[3].value for row in ws.iter_rows(min_row=2, max_col=4)]
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+            futures = {executor.submit(self.download_arxiv_pdf, pdf_url): pdf_url for pdf_url in pdf_urls}
+
+            for future in concurrent.futures.as_completed(futures):
+                pdf_url = futures[future]
+                try:
+                    future.result()
+                except Exception as e:
+                    print(f"❌ [ERROR] {pdf_url}: {e}")
+
+
+def sync_main():
+    pdf_downloader = DownloadPDF("arxiv_scraped_data.xlsx")
+    pdf_downloader.start_download()
+
+
+class ChatGPTPlaywright:
+    def __init__(self, profile_path: Optional[str] = None):
+        self.profile_path = profile_path
+
+    async def start(self):
+        self.playwright = await async_playwright().start()
+        self.browser = await self._launch_browser()
+        self.page = await self.browser.new_page()
+        await self.page.goto("https://chat.openai.com/")
+        time.sleep(10)
+        await self.page.screenshot(path="chat_gpt.png")
+
+    async def _launch_browser(self):
+        proxy = {
+        "server": "brd.superproxy.io:33335",
+        "username": "brd-customer-hl_fc29e1f2-zone-semanticscholar",
+        "password": "p8tgqocdlqav"
+        }
+        # Configure browser launch options
+        browser_args = [
+            "--no-sandbox",
+            "--disable-dev-shm-usage",
+            "--profile-directory=Profile 1",
+        ]
+
+        # Add user profile directory if provided
+        if self.profile_path:
+            abs_profile_path = os.path.expanduser(self.profile_path)
+            print(f"ℹ️ [INFO] Using profile directory: {abs_profile_path}")
+
+        browser = await self.playwright.chromium.launch_persistent_context(
+            user_data_dir=abs_profile_path if self.profile_path else None,
+            headless=True,
+            args=browser_args,
+            proxy=proxy,
+        )
+
+        return browser
+
+    async def close(self):
+        """Close the browser and stop Playwright."""
+        await self.browser.close()
+        await self.playwright.stop()
+
+if __name__ == "__main__":
+    profile_path = r"/root/arxiv-and-scholar-scraping/project1/clonedUserData"
+
+    chat_gpt = ChatGPTPlaywright()
+    asyncio.run(chat_gpt.start())
+    try:
+        print("ChatGPT page loaded successfully!")
+    finally:
+        asyncio.run(chat_gpt.close())
